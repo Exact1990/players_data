9c07f334da18691034d55530417fa31cb99df08b
Nikolay Zykov <nzykov@inno.co>
2021-04-20 15:04:45 +0500

    add relationships

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ApplicationRecord
   has_many :players
+  has_many :matches, -> (team) { unscope(:where).where("home_team_id = :team_id OR guest_team_id = :team_id", team_id: team.id) }
 end
